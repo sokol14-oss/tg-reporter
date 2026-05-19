ed80b3e43d1726569bf95568b1cce76415065e50
Anastasiya Sokolova <nastya.sokol2013@yandex.ru>
2026-05-19 15:21:39 +0400

    deleted excel.sh

--- run_report.sh
+++ run_report.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+echo "--- Запуск задачи: $(date) ---"
 cd /home/asokolova/tg-reporter
+echo "Запуск отчета..."
 /usr/bin/docker-compose run --rm reporter
+echo "Создаем бекап"
+# Создаем папку для бэкапов, если её нет
+mkdir -p ./backups
+
+cp report_main.xlsx ./backups/backup_main_$(date +%F).xlsx
+
+
+# Удаляем бэкапы старше 7 дней (чтобы не забить диск)
+find ./backups -type f -name "backup_main_*.xlsx" -mtime +7 -delete
+
+echo "--- Задача завершена: $(date) ---"
 
